f767c582f68b8e5300fb75afba237841c79803f6
Andrey Kargin <andreykargin0407@gmail.com>
2023-07-25 20:08:58 +0300

    fix loggedIn main page navigation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -152,7 +152,7 @@ function App() {
 
   return (<CurrentUserContext.Provider value={currentUser}>
     <div className='page'>
-      <Header />
+      <Header loggedIn={isLoggedIn} />
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/movies' element={<ProtectedRoute loggedIn={isLoggedIn} element={Movies} onCardDelete={handleCardDelete} handleLikeClick={handleCardLike} savedMovies={savedMovies} />} />
